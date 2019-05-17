44df5a7d1c36b50e82b43dfd553f5312537ea698
Pooslice81 <subscriptions@snyman.email>
2019-05-17 17:26:24 +1000

    3rd if statement
    Signed-off-by: Pooslice81 <subscriptions@snyman.email>

--- 00.) Basics of Programming - Part 3.py
+++ 00.) Basics of Programming - Part 3.py
@@ -2,14 +2,37 @@
 #     action
 
 
-click = False
-like = 0
-print(like)
+# click = False
+# like = 0
+# print(like)
+#
+# click = True
+#
+# if click:
+#     like += 1
+#     click = False
+#
+# print(like)
+#
+# temperature = 20
+# thermo = 15
+# print(thermo)
+#
+# if temperature <= 15:
+#     thermo = thermo + 5
+#
+# print(thermo)
+#
+# if temperature <= 20:
+#     thermo += - 3
+#
+# print(thermo)
 
-click = True
+time = "Night"
+sleepy = True
+pajamas = "Off"
 
-if click:
-    like += 1
-    click = False
+if time == "Night" and sleepy == True:
+    pajamas = "On"
 
-print(like)
+print(pajamas)
