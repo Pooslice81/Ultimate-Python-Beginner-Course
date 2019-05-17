9dec41a3983a704fb2a4dd1a6315da5d5ff36923
Pooslice81 <34494016+pooslice81@users.noreply.github.com>
2019-05-17 15:56:54 +1000

    Initial Configuration
    Signed-off-by: Pooslice81 <subscriptions@snyman.email>

--- Basics of Programming - Part 1.py
+++ Basics of Programming - Part 1.py
@@ -1,10 +1,24 @@
-one = 1
-two = 2
-three = 3
+# one = 1
+# two = 2
+# three = 3
+#
+# print(one)
+# print(two)
+# print(three)
+# two = 4
+# print(two)
+# print(one)
+#
+# decimal = 1.1
+# print(decimal)
+#
+# string_var = "Hello" + "1"
+# print(string_var)
 
-print(one)
-print(two)
-print(three)
-two = 4
-print(two)
-print(one)
+def function_name():
+    new_var = " Hello World"
+    print(new_var)
+    return
+
+function_name()
+#print(new_var)
