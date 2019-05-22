bcb679397cd8cefe2d31f271701d6a582140bf87
Pooslice81 <subscriptions@snyman.email>
2019-05-22 23:34:36 +1000

    multiple if statements inside for loops
    Signed-off-by: Pooslice81 <subscriptions@snyman.email>

--- 02.) Loops - Part 1.py
+++ 02.) Loops - Part 1.py
@@ -1,12 +1,27 @@
 word = "Hello"
 
-for letter in word:
-    print(letter)
+for x in word:
+    print(x)
 print()
 
 word_1 = "World"
+letters = []
 
 for l in word_1:
     print(l)
-    if l == "r":
-        print("This is an arrrrgh")
+    if l == "r" or l == "o":
+        print("This is an", l)
+    letters.append(l)
+
+print(letters)
+print()
+
+for x in letters:
+    print(x)
+print()
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for n in numbers:
+    if n % 2 == 1:
+        print(n)
