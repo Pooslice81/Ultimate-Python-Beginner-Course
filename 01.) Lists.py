5793dfbf477b4d9b5a3a53f4041ced6bba2c1de5
Pooslice81 <subscriptions@snyman.email>
2019-05-22 15:22:10 +1000

    updated with how to remove elements
    Signed-off-by: Pooslice81 <subscriptions@snyman.email>

--- 01.) Lists.py
+++ 01.) Lists.py
@@ -9,6 +9,8 @@ print(scores[0:2])  # print from the 1st element up to but not including the 3rd
 print(scores[1:2])  # print from the 1st element (inclusive) up to but not including the 3rd element
 print(scores[2:])   # print from the 3rd element to the end.
 
-scores[0] = 1
+scores[0] = 1       # how to reassign values of elements
 scores[1] = "A"
 print(scores)
+
+scores[0:1] = []    # how to remove list elements
