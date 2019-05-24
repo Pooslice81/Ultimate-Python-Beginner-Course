cbf81f39134199afb99f4af323c2a0f8e69e7a72
Pooslice81 <subscriptions@snyman.email>
2019-05-24 17:00:09 +1000

    While loop with a list
    Signed-off-by: Pooslice81 <subscriptions@snyman.email>

--- 03.) Loops (While) - Part 2.py
+++ 03.) Loops (While) - Part 2.py
@@ -12,3 +12,11 @@ while (counter <= 100):
     counter += 1
 
 print(sum)
+
+letters = ["a", "b", "c", "d,", "e"]
+
+index = 0
+while (index < len(letters)):
+    print(index)
+    print(letters[index])
+    index += 1
