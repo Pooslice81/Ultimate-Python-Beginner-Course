69fcd28be33b6af4f01ef91fbbd58fdf62c62f95
Pooslice81 <subscriptions@snyman.email>
2019-05-27 16:21:11 +1000

    While loop to count height to 0.
    Signed-off-by: Pooslice81 <subscriptions@snyman.email>

--- 03.) Loops (While) - Part 2.py
+++ 03.) Loops (While) - Part 2.py
@@ -4,19 +4,31 @@
 #     Action 3
 
 counter = 1
-sum = 0
+s = 0
 
-while (counter <= 100):
+while counter <= 100:
     print(counter)
-    sum += counter
+    s += counter
     counter += 1
 
-print(sum)
+print(s)
 
-letters = ["a", "b", "c", "d,", "e"]
+letters = ["a", "b", "c", "d", "e"]
 
 index = 0
-while (index < len(letters)):
+while index < len(letters):
     print(index)
     print(letters[index])
     index += 1
+print()
+
+height = 5213
+velocity = 48
+time = 0
+
+while height > 0:
+    height -= velocity
+    time += 1
+
+print(height)
+print(time)
