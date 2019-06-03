637f33257b8697df3e823cbf851409e69577079f
Johan <johan.snyman@orange.com>
2019-06-04 06:46:50 +1000

    Created a continue statement
    Signed-off-by: Johan <johan.snyman@orange.com>

--- 02.) Loops (Breaking and Continuous) - Part 3.py
+++ 02.) Loops (Breaking and Continuous) - Part 3.py
@@ -9,9 +9,22 @@ for participant in participants:
     position += 1
 
 print(position)
+print()
 
 for current_index in range(len(participants)):
+    print(current_index)
     if participants[current_index] == "Joe":
+        print("Have breaked")
         break
+    print("Not breaked")
 
-print(current_index)
+print(current_index)  # index starts at zero
+print(current_index + 1)  # to get position, add 1 to index. this is because position = 1 at the start of script
+print()
+
+for number in range(10):
+    if number % 3 == 0:
+        print(number)
+        print("Divisible by 3")
+        continue
+    print("Not divisibla by 3")
