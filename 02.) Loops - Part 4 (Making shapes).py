a3c45352d4b7ddddf88f9319d66fd7db12b294cc
Johan <johan.snyman@orange.com>
2019-06-04 07:06:11 +1000

    Larger triangle
    Signed-off-by: Johan <johan.snyman@orange.com>

--- 02.) Loops - Part 4 (Making shapes).py
+++ 02.) Loops - Part 4 (Making shapes).py
@@ -1,8 +1,8 @@
-length = 10
-to_print = "a"
+length = 20
+to_print = "o"
 
 for pos in range(1, length + 1):
     print(to_print * pos)
 
-for pos in range(length, 0, -1):
+for pos in range(length + 1, 0, -1):
     print(to_print * pos)
