09a2272ffa03d2e6b3c339c188bd9f4ec48410ec
Johan <johan.snyman@orange.com>
2019-06-05 07:14:27 +1000

    created set
    check if an element is in a set
    
    Signed-off-by: Johan <johan.snyman@orange.com>

--- 03.) Sets and Dictionaries - Part 1.py
+++ 03.) Sets and Dictionaries - Part 1.py
@@ -3,3 +3,9 @@ Sets contain each unique element.
 Dictionaries store using key-value pairs.
 Both are unpaired.
 """
+
+sets = {"element1", "element2", "element3", "element4"}
+print(sets)
+
+if "element1" in sets:
+    print("yes")
