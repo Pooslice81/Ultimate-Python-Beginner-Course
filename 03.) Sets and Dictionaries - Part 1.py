15265b57f69c0cbc1a93c47ab4f15c1332ed80db
Johan <johan.snyman@orange.com>
2019-06-05 07:19:26 +1000

    Difference between sets and lists
    Signed-off-by: Johan <johan.snyman@orange.com>

--- 03.) Sets and Dictionaries - Part 1.py
+++ 03.) Sets and Dictionaries - Part 1.py
@@ -9,3 +9,13 @@ print(sets)
 
 if "element1" in sets:
     print("yes")
+
+country_list = []
+for i in range(5):
+    country = input("Please enter your country: ")
+    country_list.append(country)
+
+country_set = set(country_list)
+
+print(country_list)
+print(country_set)
