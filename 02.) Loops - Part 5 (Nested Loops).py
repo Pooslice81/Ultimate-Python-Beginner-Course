94012fb31d2d93e008e759923633957887baccaa
Johan <johan.snyman@orange.com>
2019-06-05 07:03:09 +1000

    Print a tic-tac-toe board using nested loops.
    Signed-off-by: Johan <johan.snyman@orange.com>

--- 02.) Loops - Part 5 (Nested Loops).py
+++ 02.) Loops - Part 5 (Nested Loops).py
@@ -2,14 +2,29 @@
 Adding flow control into loops
 Print custom output using nested loops. '''
 
-# | |   0
-# -----  1
-# | |   2
-# -----  3
-# | |   4
+#  | |   0 1
+# -----  1 2
+#  | |   2 3
+# -----  3 4
+#  | |   4 5
 
 for row in range(5):  # 0,1 ,2, 3, 4
     if row % 2 == 0:
         print(" | | ")
+    else:
+        print("-----")
+print()
+print()
+
+for row in range(5):
+    if row % 2 == 0:
+        for column in range(1, 6):
+            if column % 2 == 1:
+                if column != 5:
+                    print(" ", end="")
+                else:
+                    print(" ")
+            else:
+                print("|", end="")
     else:
         print("-----")
