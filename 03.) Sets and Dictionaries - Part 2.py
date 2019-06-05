0bc9182be5d60d646cb15e4054bb242f88d0a8cb
Johan <johan.snyman@orange.com>
2019-06-06 06:59:34 +1000

    created program. doesn't cater for negative values
    Signed-off-by: Johan <johan.snyman@orange.com>

--- 03.) Sets and Dictionaries - Part 2.py
+++ 03.) Sets and Dictionaries - Part 2.py
@@ -4,3 +4,14 @@
 - We can easily access desired values (with their keys)
   without having to remember their original position (like on a list)
 """
+
+black_shoes = {42: 2,
+               41: 3,
+               40: 4,
+               39: 1,
+               38: 0}
+
+while True:
+    purchase_size = int(input("What shoe size would you like to buy? \n"))
+    black_shoes[purchase_size] -= 1
+    print(black_shoes)
