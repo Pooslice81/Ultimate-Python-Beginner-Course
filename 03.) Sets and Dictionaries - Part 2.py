d539c1e22d2a8338ff888c5a70809c27d60cbf77
Johan <johan.snyman@orange.com>
2019-06-06 07:03:02 +1000

    Amended to cater for negative stock values
    Signed-off-by: Johan <johan.snyman@orange.com>

--- 03.) Sets and Dictionaries - Part 2.py
+++ 03.) Sets and Dictionaries - Part 2.py
@@ -5,13 +5,16 @@
   without having to remember their original position (like on a list)
 """
 
-black_shoes = {42: 2,
-               41: 3,
-               40: 4,
-               39: 1,
-               38: 0}
+black_shoes = {42:2,
+               41:3,
+               40:4,
+               39:1,
+               38:0}
 
 while True:
     purchase_size = int(input("What shoe size would you like to buy? \n"))
-    black_shoes[purchase_size] -= 1
+    if black_shoes[purchase_size] > 0:
+        black_shoes[purchase_size] -= 1
+    else:
+        print("Shoes are no longer in stock.")
     print(black_shoes)
