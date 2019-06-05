0e312e9071584b3d5103aa2c742ac88b1645d0ba
Johan <johan.snyman@orange.com>
2019-06-06 06:46:27 +1000

    Docstring
    Signed-off-by: Johan <johan.snyman@orange.com>

---
+++ 03.) Sets and Dictionaries - Part 2.py
@@ -0,0 +1,6 @@
+"""
+- Practical application of dictionaries
+- Keeping track of unique item numbers
+- We can easily access desired values (with their keys)
+  without having to remember their original position (like on a list)
+"""
