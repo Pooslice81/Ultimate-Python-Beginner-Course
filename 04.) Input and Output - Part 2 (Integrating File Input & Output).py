e27e9b1ead94a225b125a465ddf09ee8c867fc5c
Johan <johan.snyman@orange.com>
2019-06-11 21:01:00 +1000

    create a file that writes and reads city names.
    Signed-off-by: Johan <johan.snyman@orange.com>

--- 04.) Input and Output - Part 2 (Integrating File Input & Output).py
+++ 04.) Input and Output - Part 2 (Integrating File Input & Output).py
@@ -3,3 +3,25 @@
 - We can save our data to files by writing to them.
 - Format start with open and includes what we want to do, and closing the file after we've finished with it.
 """
+
+# file = open("filename", "r/w/a/r+")
+# file.close()
+
+cities = ["London\n", "Paris\n", "New York\n", "Los Angeles\n", "Cape Town\n"]
+
+cities_file = open("cities", "w")
+
+for city in cities:
+    cities_file.write(city)
+
+print("Done")
+
+cities_file.close()
+
+cities_file = open("cities", "r")
+
+the_whole_file = cities_file.read()
+
+print(the_whole_file)
+
+cities_file.close()
