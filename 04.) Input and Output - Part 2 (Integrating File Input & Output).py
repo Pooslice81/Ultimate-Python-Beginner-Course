c15399ded2b00a6bcc458437d42a3486eb8e94af
Johan <johan.snyman@orange.com>
2019-06-11 20:35:05 +1000

    docstring
    Signed-off-by: Johan <johan.snyman@orange.com>

---
+++ 04.) Input and Output - Part 2 (Integrating File Input & Output).py
@@ -0,0 +1,5 @@
+"""
+- How can we import data from files on our computer
+- We can save our data to files by writing to them.
+- Format start with open and includes what we want to do, and closing the file after we've finished with it.
+"""
